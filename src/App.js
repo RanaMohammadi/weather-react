2dc27182139762de5a222bc5e5b85aeabf9f74f5
RanaMohammadi <itsmerana.m@gmail.com>
2022-11-17 19:27:27 +0100

    added github link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+
       <Search />
+      <p>
+        Coded by
+        <a
+          href="https://github.com/RanaMohammadi/weather-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Rana Mohammadi
+        </a>
+      </p>
     </div>
   );
 }
