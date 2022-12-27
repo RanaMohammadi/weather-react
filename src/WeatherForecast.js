1dc21b40b483c253522fdb022399fed7a39f9d3d
RanaMohammadi <itsmerana.m@gmail.com>
2022-12-27 01:53:09 +0100

    minor fix

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -24,6 +24,8 @@ export default function WeatherForecast(props) {
                   <WeatheForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
